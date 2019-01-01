45afc247f31877d6edb928bdec21b027179da612
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-01 22:12:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
     #self.count(".") + self.count("!") + self.count("?")
-    self.split.each {|word| word.end_with?("!", ".", "?")}.delete_if(false).length
+    self.split.each {|word| word.end_with?("!", ".", "?")}.delete_if("false").length
   end
 end
