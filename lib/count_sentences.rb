7694d8de4b5ec4e7ea7a44fc680eaf9b84288cd0
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-01 22:14:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #self.count(".") + self.count("!") + self.count("?")
+    self.split.map {|word| word.end_with?("!", ".", "?")}.count(true)
   end
 end
