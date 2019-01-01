b9b7628d8c79b62c89d3c2b2189d44c9081773e6
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-01 22:10:14 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
     #self.count(".") + self.count("!") + self.count("?")
-    self.split.map {|word| word.end_with?("!", ".", "?")}
+    self.split.each {|word| word.end_with?("!", ".", "?")}
   end
 end
